ffc337a04bbb875a7b89d79c9e8cf580b34af4a0
Ali Hussein <hussein794@gmail.com>
2023-01-21 15:28:48 +0000

    Game over 1080

--- DimensionRun/Code/State_Game.cpp
+++ DimensionRun/Code/State_Game.cpp
@@ -41,13 +41,13 @@ void State_Game::OnCreate() {
 	randomMusic = 1 + (rand() % 3);
 
 	if (randomMusic == 1) {
-		m_StateMgr->GetContext()->m_SoundManager->PlayMusic("InGameMusic1", 40.f, true);
+		m_StateMgr->GetContext()->m_SoundManager->PlayMusic("InGameMusic1", 30.f, true);
 	}
 	else if (randomMusic == 2) {
-		m_StateMgr->GetContext()->m_SoundManager->PlayMusic("InGameMusic2", 40.f, true);
+		m_StateMgr->GetContext()->m_SoundManager->PlayMusic("InGameMusic2", 30.f, true);
 	}
 	else if (randomMusic == 3) {
-		m_StateMgr->GetContext()->m_SoundManager->PlayMusic("InGameMusic3", 40.f, true);
+		m_StateMgr->GetContext()->m_SoundManager->PlayMusic("InGameMusic3", 30.f, true);
 	}
 }
 
